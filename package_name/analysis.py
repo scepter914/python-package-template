d719e1fa74dcf472158ff23658440809677116e1
scepter914 <scepter914@gmail.com>
2022-07-25 16:49:23 +0900

    add subplot interface
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- package_name/analysis.py
+++ package_name/analysis.py
@@ -3,14 +3,14 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-from package_name.util.plot import get_lim
+from package_name.util.plot import get_grid_interval, get_lim
 
 
 class TestData:
     def __init__(self) -> None:
-        self.t = np.linspace(10.0, 16.28, 628)
-        self.y1 = np.sin(self.t)
-        self.y2 = np.cos(self.t)
+        self.t: np.ndarray = np.linspace(10.0, 16.28, 628)
+        self.y1: np.ndarray = np.sin(self.t)
+        self.y2: np.ndarray = np.cos(self.t)
 
 
 class Analyzer:
@@ -26,28 +26,34 @@ class Analyzer:
         os.makedirs(figure_directory_path, exist_ok=True)
         self.figure_directory_path: str = figure_directory_path
 
-    def plot(self, data: TestData) -> None:
+    def plot(self, data_: TestData) -> None:
+        self._subplot(
+            data=data_,
+            title="Title",
+            x_label="time [sec]",
+            y_label="position [m]",
+        )
+
+    def _subplot(
+        self,
+        data: TestData,
+        title: str,
+        x_label: str,
+        y_label: str,
+    ) -> None:
         # setting figure
         fig, ax = plt.subplots(figsize=(12, 8))
-        ax.set_title("Title", fontsize=30)
-        ax.set_xlabel("time [sec]", fontsize=20)
-        ax.set_ylabel("position [m]", fontsize=20)
+        ax.set_title(title, fontsize=30)
+        ax.set_xlabel(x_label, fontsize=20)
+        ax.set_ylabel(y_label, fontsize=20)
+
+        # set data
+        data_x: np.ndarray = data.t
+        plot_size: float = 150.0 / len(data_x)
 
         # plot
-        ax.scatter(
-            data.t,
-            data.y1,
-            color="red",
-            s=1.0,
-            label="sin",
-        )
-        ax.scatter(
-            data.t,
-            data.y2,
-            color="blue",
-            s=1.0,
-            label="cos",
-        )
+        ax.scatter(data_x, data.y1, color="red", s=plot_size, label="sin")
+        ax.scatter(data_x, data.y2, color="blue", s=plot_size, label="cos")
         # ax.plot(data.t, data.y1, color="red", label="sin")
         # ax.plot(data.t, data.y2, color="blue", label="cos")
 
@@ -55,14 +61,13 @@ class Analyzer:
         ax.legend(loc=(0.05, 0.05), fontsize=14)
 
         # setting axis
-        x_interval: float = 1.0
-        y_interval: float = 0.1
+        x_interval: float = get_grid_interval(data_x)
+        y_interval: float = max(get_grid_interval(data.y1), get_grid_interval(data.y2))
 
         x_max: float
         x_min: float
         y_max: float
         y_min: float
-
         x_max, x_min = get_lim(
             value_list=data.t,
             interval=x_interval,
