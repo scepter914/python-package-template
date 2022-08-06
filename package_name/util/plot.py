d45507652d4bbe53099472e827a9a51585157c2b
scepter914 <scepter914@gmail.com>
2022-08-06 17:00:40 +0900

    add get_grid_interval_from_multiple_values
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- package_name/util/plot.py
+++ package_name/util/plot.py
@@ -2,26 +2,9 @@ import math
 from typing import List, Tuple
 
 
-def get_lim(
-    value_list: List[float],
-    interval: float,
-) -> Tuple[float, float]:
-    """Get limit value for set_lim function of matplotlib
-    Args:
-        value_list (List[float]): The value list to plot.
-        interval (float): The interval value for grid
-
-    Returns:
-        Tuple[float, float]: [max_limit, min_limit] for set_xlim and set_ylim
-    """
-    min_lim = _get_min_lim(min(value_list), interval)
-    max_lim = _get_max_lim(max(value_list), interval)
-    return (max_lim, min_lim)
-
-
 def get_lim_from_multiple_values(
     value_lists: List[List[float]],
-    interval_: float,
+    interval: float,
 ) -> Tuple[float, float]:
     """Get limit value from multiple value lists for set_lim function of matplotlib
     Args:
@@ -38,11 +21,28 @@ def get_lim_from_multiple_values(
     min_lim: float
 
     for value_list_ in value_lists:
-        max_lim, min_lim = get_lim(value_list=value_list_, interval=interval_)
+        max_lim, min_lim = get_lim(value_list=value_list_, interval=interval)
         max_lim_list.append(max_lim)
         min_lim_list.append(min_lim)
 
     return max(max_lim_list), min(min_lim_list)
+
+
+def get_lim(
+    value_list: List[float],
+    interval: float,
+) -> Tuple[float, float]:
+    """Get limit value for set_lim function of matplotlib
+    Args:
+        value_list (List[float]): The value list to plot.
+        interval (float): The interval value for grid
+
+    Returns:
+        Tuple[float, float]: [max_limit, min_limit] for set_xlim and set_ylim
+    """
+    min_lim = _get_min_lim(min(value_list), interval)
+    max_lim = _get_max_lim(max(value_list), interval)
+    return (max_lim, min_lim)
 
 
 def _get_max_lim(
@@ -87,6 +87,21 @@ def _get_min_lim(
     if rounded_value >= min_value:
         rounded_value -= interval
     return rounded_value
+
+
+def get_grid_interval_from_multiple_values(value_lists: List[List[float]]) -> float:
+    """Get grid interval value for plot from multiple values list
+
+    Args:
+        value_lists (List[List[float]]): Plot values lists
+
+    Returns:
+        float: Grid interval value for plot
+    """
+    interval_values = []
+    for values in value_lists:
+        interval_values.append(get_grid_interval(values))
+    return max(interval_values)
 
 
 def get_grid_interval(value_list: List[float]) -> float:
